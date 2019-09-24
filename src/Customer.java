cfb68f7fbb5754c0410cb5aa49b64b255ced1d33
Ludivine Rousseau <ludivine.rousseau@outlook.fr>
2019-09-24 13:35:40 +0200

    ajout du contructeur pour le client

--- src/Customer.java
+++ src/Customer.java
@@ -2,7 +2,11 @@
 public class Customer {
     private String fullname;
     private String address;
-
+    public Customer(String fullname, String address) {
+    	this.fullname = fullname;
+    	this.address = address;
+    }
+    
     public String getFullname() {
         return fullname;
     }
