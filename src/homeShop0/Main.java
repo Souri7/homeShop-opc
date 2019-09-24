5fc64e2ee276f3618518cd2e419168ad247f57f4
Ludivine Rousseau <ludivine.rousseau@outlook.fr>
2019-09-24 20:09:31 +0200

    ajout contenu main

--- src/homeShop0/Main.java
+++ src/homeShop0/Main.java
@@ -1,3 +1,16 @@
 package homeShop0;
+
 public class Main {
+	public static void main(String[] arg) {
+	Product cafe = new Product("senseo", "noire, une ou deux tasse", 45 );
+	Fridge frigo = new Fridge ("curtiss", "blanc, 130L, A+", 299 , 130, true);
+	
+	Customer client = new Customer ("jean jean", "ici ou la bas");
+	
+	Bill bill = new Bill (client, null);
+	
+	bill.addProduct(frigo, 2);
+	bill.addProduct(cafe, 1);
+	
+}
 }
